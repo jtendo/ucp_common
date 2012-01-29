ddf2ab55cd5baf9843280bc1d8b5791941b28d05
Adam Rutkowski <hq@mtod.org>
2012-01-29 20:24:06 +0100

    Add buffer-aware packet decoder.

--- src/ucp_framing.erl
+++ src/ucp_framing.erl
@@ -1,35 +1,53 @@
 -module(ucp_framing).
 -author('adam.rutkowski@jtendo.com').
 
--export([recv_ucp_sock/1]).
+-export([try_decode/2]).
 
 -include("logger.hrl").
 -include("ucp_syntax.hrl").
 
-recv_ucp_sock(S) ->
-    case wait_for_frame(S, []) of
-        {packet, Data} ->
-            ?SYS_DEBUG("Received frame: ~p", [Data]),
-            {ok, Data};
-        Other ->
-            ?SYS_WARN("Could not recognize frame: ~p", [Other]),
-            Other
+-define(BEGIN, ?STX).
+-define(END,   ?ETX).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                   API                                   %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+try_decode(Incoming, Buffered) when is_binary(Incoming) ->
+    try_decode(Incoming, [], [], Buffered).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                        Packet framing functions                         %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+try_decode(<<>>, Buffer, Collected) ->
+    IOL = lists:reverse(Collected),
+    {lists:map(fun iolist_to_binary/1, IOL), lists:reverse(Buffer)};
+
+try_decode(Incoming, Buffer, Collected) ->
+    case binary:split(Incoming, <<?END>>) of
+        [M = <<?BEGIN, _Body/binary>>, <<>>] ->
+            %% looks like we're done
+            try_decode(<<>>, Buffer, [[M, ?END]|Collected]);
+        [M = <<?BEGIN, _Body/binary>>, R = <<?BEGIN, _More/binary>>] ->
+            %% one message decoded, try remaining
+            try_decode(R, Buffer, [[M, ?END]|Collected]);
+        [Incomplete] ->
+            %% apparently packet fragmenting occured;
+            %% we can't handle that, so make it go to the buffer
+            try_decode(<<>>, [Incomplete|Buffer], Collected);
+        [Incomplete, More] ->
+            %% most likely we have a binary closing the previously
+            %% fragmented packet. buffer it and inspect remaining bytes.
+            try_decode(More, [Incomplete|Buffer], Collected)
     end.
 
-%% FIXME
-wait_for_frame(S, Buff) ->
-    {ok, Data} = gen_tcp:recv(S, 1),
-    case Data of
-            B = <<?STX>> ->
-                wait_for_frame(S, [B|Buff]);
-            B = <<?ETX>> ->
-                classify_packet(lists:reverse([B|Buff]));
-            Byte ->
-                wait_for_frame(S, [Byte|Buff])
-    end.
+try_decode(Incoming, [], [], [L, R]) ->
+    try_decode(iolist_to_binary([L, Incoming, R]), [], []);
 
-classify_packet(P = [<<?STX>>|_]) ->
-    {packet, iolist_to_binary(P)};
-classify_packet(P) ->
-    {malformed, iolist_to_binary(P)}.
+try_decode(Incoming, [], [], [L]) ->
+    try_decode(iolist_to_binary([L, Incoming]), [], []);
+
+try_decode(Incoming, [], [], B) when B =:= []; B =:= undefined ->
+    try_decode(Incoming, [], []).
 
