b3a52b9985d4d5e3df9d089075cb5bedb1e274ef
Adam Rutkowski <hq@mtod.org>
2012-01-29 21:42:28 +0100

    Shape the module. Minor fixes and additions:
    Fix invalid buffer return.
    Add ucp_framing tests.
    Fix ucp_utils test calls.
    Add copyright notice.
    Provide basic API typespecs.

--- src/ucp_framing.erl
+++ src/ucp_framing.erl
@@ -1,6 +1,22 @@
 -module(ucp_framing).
 -author('adam.rutkowski@jtendo.com').
 
+% A basic utlility for fragmented UCP packets reassembly.
+
+% Copyright (c) 2012, jtendo
+
+% Permission to use, copy, modify, and/or distribute this software for any
+% purpose with or without fee is hereby granted, provided that the above
+% copyright notice and this permission notice appear in all copies.
+
+% THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+% WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+% MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+% ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+% WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+% ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+% OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.OF SUCH DAMAGE.
+
 -export([try_decode/2]).
 
 -include("logger.hrl").
@@ -9,9 +25,18 @@
 -define(BEGIN, ?STX).
 -define(END,   ?ETX).
 
+-type ucp_buff() :: undefined | [binary()]. % FIXME, should be a tuple as we
+                                            % expect it to be [0..2] elements
+-type ucp_pkt() :: [binary()].
+
+-export_type([ucp_buff/0, ucp_pkt/0]).
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                                   API                                   %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+-spec try_decode(binary(), Buffered :: ucp_buff()) ->
+    {ucp_pkt(), ToBeBuffered :: ucp_buff()}.
 
 try_decode(Incoming, Buffered) when is_binary(Incoming) ->
     try_decode(Incoming, [], [], Buffered).
@@ -21,8 +46,10 @@ try_decode(Incoming, Buffered) when is_binary(Incoming) ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 try_decode(<<>>, Buffer, Collected) ->
-    IOL = lists:reverse(Collected),
-    {lists:map(fun iolist_to_binary/1, IOL), lists:reverse(Buffer)};
+    IOL_Col = lists:reverse(Collected),
+    IOL_Buf = lists:reverse(Buffer),
+    {lists:map(fun iolist_to_binary/1, IOL_Col),
+     lists:map(fun iolist_to_binary/1, IOL_Buf)};
 
 try_decode(Incoming, Buffer, Collected) ->
     case binary:split(Incoming, <<?END>>) of
@@ -39,7 +66,7 @@ try_decode(Incoming, Buffer, Collected) ->
         [Incomplete, More] ->
             %% most likely we have a binary closing the previously
             %% fragmented packet. buffer it and inspect remaining bytes.
-            try_decode(More, [Incomplete|Buffer], Collected)
+            try_decode(More, [[Incomplete, ?END]|Buffer], Collected)
     end.
 
 try_decode(Incoming, [], [], [L, R]) ->
@@ -51,3 +78,65 @@ try_decode(Incoming, [], [], [L]) ->
 try_decode(Incoming, [], [], B) when B =:= []; B =:= undefined ->
     try_decode(Incoming, [], []).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                               Eunit Tests                               %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-ifdef (TEST).
+-include_lib("eunit/include/eunit.hrl").
+decoder_test_() ->
+    [
+        {"Extract single UCP packet from single UCP packet :)", fun complete/0},
+        {"Assume it's right fragmentation", fun unknown_packet/0},
+        {"Extract UCP packet(s) when both ends are fragments",
+            [fun fragment_both/0,
+             fun fragment_both_2/0]},
+        {"Extract UCP packet(s) when both ends are fragments but there are"
+         "buffered data available",
+         [fun fragment_with_buffer/0,
+          fun fragment_with_buffer_2/0]},
+        {"Extract UCP packet(s) with presence of a two-element buffer",
+            fun fragment_with_buffer_3/0}
+    ].
+
+complete() ->
+    B = <<?BEGIN, 0,0,0,0,0, ?END>>,
+    ?assertEqual(
+        {[B],[]}, try_decode(B, [])
+    ).
+unknown_packet() ->
+    B = <<0,0,0>>,
+    ?assertEqual(
+        {[], [B]}, try_decode(B, [])
+    ).
+fragment_both() ->
+    B = <<0,0,?END,?BEGIN,0,0,0,0,?END,?BEGIN,0,0>>,
+    ?assertEqual(
+        {[<<?BEGIN,0,0,0,0,?END>>], [<<0,0,?END>>, <<?BEGIN,0,0>>]},
+        try_decode(B, [])
+    ).
+fragment_both_2() ->
+    B = <<0,0,?END,?BEGIN,0,0,?END,?BEGIN,0,0,?END,?BEGIN>>,
+    ?assertEqual(
+        {[<<?BEGIN,0,0,?END>>,<<?BEGIN,0,0,?END>>], [<<0,0,?END>>,
+                <<?BEGIN>>]},
+        try_decode(B, [])
+    ).
+fragment_with_buffer() ->
+    B = <<0,0,?END,?BEGIN,0,0,0,?END>>,
+    ?assertEqual(
+        {[<<?BEGIN,1,1,0,0,?END>>, <<?BEGIN,0,0,0,?END>>], []},
+        try_decode(B, [<<?BEGIN,1,1>>])
+    ).
+fragment_with_buffer_2() ->
+    B = <<0,0,?END,?BEGIN,0,0,0,?END,?BEGIN>>,
+    ?assertEqual(
+        {[<<?BEGIN,1,1,0,0,?END>>, <<?BEGIN,0,0,0,?END>>], [<<?BEGIN>>]},
+        try_decode(B, [<<?BEGIN,1,1>>])
+    ).
+fragment_with_buffer_3() ->
+    B = <<0,0,?END,?BEGIN,0,?END,?BEGIN>>,
+    ?assertEqual(
+        {[<<?BEGIN,1,0,0,?END>>,<<?BEGIN,0,?END>>,<<?BEGIN,1,1,0,?END>>], []},
+        try_decode(B, [<<?BEGIN,1>>, <<1,1,0,?END>>])
+    ).
+-endif.
--- src/ucp_utils.erl
+++ src/ucp_utils.erl
@@ -387,31 +387,31 @@ encode_reverse([A, B|T], Acc) ->
 
 split_join_test() ->
     L = "ABC/DEF/GHI",
-    R = ucp_utils:ucp_split(L),
+    R = ucp_split(L),
     ?assertEqual(["ABC", "DEF", "GHI"], R),
-    L2 = ucp_utils:ucp_join(R, R),
+    L2 = ucp_join(R, R),
     ?assertEqual("ABC/DEF/GHI/ABC/DEF/GHI", L2),
-    L3 = ucp_utils:ucp_join(R, R, append),
+    L3 = ucp_join(R, R, append),
     ?assertEqual("ABC/DEF/GHI/ABC/DEF/GHI/", L3),
-    R2 = ucp_utils:ucp_split(L3),
+    R2 = ucp_split(L3),
     ?assertEqual(["ABC", "DEF", "GHI", "ABC", "DEF", "GHI", []], R2),
-    R3 = ucp_utils:ucp_split("///"),
+    R3 = ucp_split("///"),
     ?assertEqual([[],[],[],[]], R3).
 
 rv_test() ->
     A = {foo, bar, baz},
     B = {foo, [bar, {baz}]},
-    ?assertEqual([bar, baz], ucp_utils:rv(A)),
-    ?assertEqual([[bar, {baz}]], ucp_utils:rv(B)).
+    ?assertEqual([bar, baz], rv(A)),
+    ?assertEqual([[bar, {baz}]], rv(B)).
 
 encode_sender_test() ->
-    ?assertEqual({"", "1112376382900"}, ucp_utils:encode_sender("1112376382900")),
-    ?assertEqual({"5039", "106F79D87D2EBBE06C"}, ucp_utils:encode_sender("orange.pl")),
-    ?assertEqual("orange.pl", ucp_utils:decode_sender("5039", "106F79D87D2EBBE06C")),
-    ?assertEqual("11112376382900", ucp_utils:decode_sender(whatever, "11112376382900")),
+    ?assertEqual({"", "1112376382900"}, encode_sender("1112376382900")),
+    ?assertEqual({"5039", "106F79D87D2EBBE06C"}, encode_sender("orange.pl")),
+    ?assertEqual("orange.pl", decode_sender("5039", "106F79D87D2EBBE06C")),
+    ?assertEqual("11112376382900", decode_sender(whatever, "11112376382900")),
     ?assertEqual({"5039", "2721E08854F29A54A854ABB7DA76F77DEECBC5D201"},
-        ucp_utils:encode_sender("!@#$%^&*()-=+[]{}\\/.,:")),
-    ?assertEqual("!@#$%^&*()-=+[]{}\\/.,:", ucp_utils:decode_sender("5039",
+        encode_sender("!@#$%^&*()-=+[]{}\\/.,:")),
+    ?assertEqual("!@#$%^&*()-=+[]{}\\/.,:", decode_sender("5039",
         "2721E08854F29A54A854ABB7DA76F77DEECBC5D201")).
 
 -endif.
